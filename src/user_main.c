a7170e238d192341186a4f7acf5eb0050445e860
openshwprojects <openshwprojects@gmail.com>
2022-04-27 14:47:08 +0200

    added console command to set arbitrary GET request to any server, but dont use it yet, because it seems there is a 88 bytes memory leak somewhere in the http client library (it also happens once when doing OTA but then device restarts so no one cares)

--- src/user_main.c
+++ src/user_main.c
@@ -400,6 +400,7 @@ void Main_Init()
 		taslike_commands_init();
 		fortest_commands_init();
 		NewLED_InitCommands();
+		CMD_InitSendCommands();
 		CMD_InitChannelCommands();
 		EventHandlers_Init();
 
